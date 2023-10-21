aa253a05a50380713ef04a1c41ef3f54bfe05ad9
Guillermina Manzano <manzanog0612@gmail.com>
2023-10-21 16:59:37 -0300

    Re-add project (last one wasn't correct project).

---
+++ Assets/Scripts/Tank/Tank.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+
+public class Tank : TankBase
+{
+    float fitness = 0;
+    protected override void OnReset()
+    {
+        fitness = 1;
+    }
+
+    protected override void OnThink(float dt)
+    {
+        Vector3 dirToGoodMine = GetDirToObject(goodMine);
+        Vector3 dirToBadMine = GetDirToObject(badMine);
+        Vector3 dirCloserTank = GetDirToObject(nearTank);
+
+        float distanceToGoodMine = GetDistToObject(goodMine);
+        float distanceToBadMine = GetDistToObject(badMine);
+        float distanceToCloserTank = GetDistToObject(nearTank);
+
+        inputs[0] = dirToGoodMine.x;
+        inputs[1] = dirToGoodMine.z;
+        inputs[2] = distanceToGoodMine;
+        inputs[3] = dirToBadMine.x;
+        inputs[4] = dirToBadMine.z;
+        inputs[5] = distanceToBadMine;
+        inputs[6] = transform.forward.x;
+        inputs[7] = transform.forward.z;
+        inputs[8] = dirCloserTank.x;
+        inputs[9] = dirCloserTank.z;
+        inputs[10] = distanceToCloserTank;
+
+        float[] output = brain.Synapsis(inputs);
+
+        switch (PopulationManager.Instance.testIndex)
+        {
+            case 0:
+            case 1:
+                if (distanceToGoodMine < 4)
+                {
+                    fitness += 1f;
+
+                    if (distanceToGoodMine < 3)
+                    {
+                        fitness += 5f;
+                    }
+                }
+                break;
+            default:
+                if (IsCollidingWithTank())
+                {
+                    fitness -= 0.3f;
+                }
+                break;
+        }
+
+        genome.fitness = fitness;
+
+        SetForces(output[0], output[1], dt);
+    }
+
+    protected override void OnTakeMine(GameObject mine)
+    {
+        switch (PopulationManager.Instance.testIndex)
+        {
+            case 0:
+                fitness *= 2;
+                genome.fitness = fitness;
+                break;
+            case 1:
+                if (IsGoodMine(mine))
+                {
+                    fitness *= 2;
+                }
+                else
+                {
+                    fitness /= 2;
+                }
+                genome.fitness = fitness;
+                break;
+            default:
+                break;
+        } 
+    }
+}
---
+++ Assets/Scripts/Tank/TankBase.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+
+public class TankBase : MonoBehaviour
+{
+    public float Speed = 4.0f;
+    public float RotSpeed = 20.0f;
+
+    protected Genome genome;
+	protected NeuralNetwork brain;
+    protected GameObject nearMine;
+    protected GameObject goodMine;
+    protected GameObject badMine;
+    protected GameObject nearTank;
+    protected float[] inputs;
+
+    public void SetBrain(Genome genome, NeuralNetwork brain)
+    {
+        this.genome = genome;
+        this.brain = brain;
+        inputs = new float[brain.InputsCount];
+        OnReset();
+    }
+
+    public void SetNearestMine(GameObject mine)
+    {
+        nearMine = mine;
+    }
+
+    public void SetGoodNearestMine(GameObject mine)
+    {
+        goodMine = mine;
+    }
+
+    public void SetBadNearestMine(GameObject mine)
+    {
+        badMine = mine;
+    }
+
+    public void SetNearestTank(GameObject tank)
+    {
+        nearTank = tank;
+    }
+
+    public bool IsCollidingWithTank()
+    {
+        return (this.transform.position - nearTank.transform.position).sqrMagnitude <= 2.0f;
+    }
+
+    protected bool IsGoodMine(GameObject mine)
+    {
+        return goodMine == mine;
+    }
+
+    protected Vector3 GetDirToObject(GameObject obj)
+    {
+        return (obj.transform.position - this.transform.position).normalized;
+    }
+
+    protected float GetDistToObject(GameObject obj)
+    {
+        return Vector3.Distance(obj.transform.position, transform.position);
+    }
+
+    protected bool IsCloseToMine(GameObject mine)
+    {
+        return (this.transform.position - nearMine.transform.position).sqrMagnitude <= 2.0f;
+    }
+
+    protected void SetForces(float leftForce, float rightForce, float dt)
+    {
+        Vector3 pos = transform.position;
+        float rotFactor = Mathf.Clamp((rightForce - leftForce), -1.0f, 1.0f);
+        transform.rotation *= Quaternion.AngleAxis(rotFactor * RotSpeed * dt, Vector3.up);
+        pos += transform.forward * Mathf.Abs(rightForce + leftForce) * 0.5f * Speed * dt;
+        transform.position = pos;
+    }
+
+	public void Think(float dt) 
+	{
+        OnThink(dt);
+
+        if(IsCloseToMine(nearMine))
+        {
+            OnTakeMine(nearMine);
+            PopulationManager.Instance.RelocateMine(nearMine);
+        }
+	}
+
+    protected virtual void OnThink(float dt)
+    {
+
+    }
+
+    protected virtual void OnTakeMine(GameObject mine)
+    {
+    }
+
+    protected virtual void OnReset()
+    {
+
+    }
+}
